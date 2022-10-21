e809838c048ae3cb76acf1b099ac7231651e9e92
Maxim Korovin <fazamkg@gmail.com>
2022-10-22 04:45:40 +0500

    Adding win condition and ui for it

--- Assets/Scripts/Player/Network/ScoreTaker.cs
+++ Assets/Scripts/Player/Network/ScoreTaker.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
+using System;
 
 public class ScoreTaker : NetworkBehaviour
 {
-	[SerializeField] private Text _scoreText; 
+	[SerializeField] private Text _scoreText;
+	[SerializeField] private Text _winText;
+
+	[SerializeField] private int _scoreToWin = 3;
 
 	private int _score;
 
+	private static bool _somebodyWon;
+
+	private static event Action<string> OnPlayerWon;
+
+	public override void OnStartAuthority()
+	{
+		base.OnStartAuthority();
+
+		OnPlayerWon += ShowWinUI;
+	}
+
+	private void ShowWinUI(string message)
+	{
+		_winText.gameObject.SetActive(true);
+		_winText.text = message;
+	}
+
 	[Client]
-	private void UpdateUI()
+	private void UpdateScoreUI()
 	{
 		if (!isLocalPlayer) return;
 		_scoreText.text = _score.ToString();
@@ -19,13 +40,32 @@ public class ScoreTaker : NetworkBehaviour
 	private void RpcUpdateScoreOnClients(int score)
 	{
 		_score = score;
-		UpdateUI();
+		UpdateScoreUI();
+	}
+
+	[ClientRpc]
+	private void RpcNotifyClientsAboutWinner(NetworkIdentity winner)
+	{
+		_somebodyWon = true;
+
+		OnPlayerWon?.Invoke($"[{winner.netId}] wins the game!");
+	}
+	
+	[Server]
+	private void CheckIfPlayerWon()
+	{
+		if (!_somebodyWon && _score >= _scoreToWin)
+		{
+			_somebodyWon = true;
+			RpcNotifyClientsAboutWinner(netIdentity);
+		}
 	}
 
 	[Server]
 	public void AddScore(int score)
 	{
 		_score += score;
+		CheckIfPlayerWon();
 		RpcUpdateScoreOnClients(_score);
 	}
 }
